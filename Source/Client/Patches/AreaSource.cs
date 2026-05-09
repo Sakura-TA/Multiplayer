02f60caaae3ca02147852751c5f648532cc12845
Sakura-TA <Sakura_TA@163.com>
2026-05-09 10:07:15 +0800

    Replace stacked harmony patch atttribute with TargetMethods

--- Source/Client/Patches/AreaSource.cs
+++ Source/Client/Patches/AreaSource.cs
@@ -2,26 +2,27 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
 
 namespace Multiplayer.Client.Patches
 {
-    [HarmonyPatch(typeof(AreaSource))]
+    [HarmonyPatch]
     static class AreaSource_Patch
     {
-        [HarmonyPatch(nameof(AreaSource.ComputeAll))]
-        [HarmonyPatch(nameof(AreaSource.UpdateIncrementally))]
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.DeclaredMethod(typeof(AreaSource), nameof(AreaSource.ComputeAll));
+            yield return AccessTools.DeclaredMethod(typeof(AreaSource), nameof(AreaSource.UpdateIncrementally));
+        }
         static void Prefix(AreaSource __instance, ref AreaManager __state)
         {
             if (Multiplayer.Client == null || !Multiplayer.GameComp.multifaction) return;
             __state = __instance.map.areaManager;
             __instance.map.areaManager = __instance.map.MpComp().AllAreaManager();
         }
-
-        [HarmonyPatch(nameof(AreaSource.ComputeAll))]
-        [HarmonyPatch(nameof(AreaSource.UpdateIncrementally))]
         static void Finalizer(AreaSource __instance, AreaManager __state)
         {
             if (Multiplayer.Client == null || !Multiplayer.GameComp.multifaction) return;
